274f747d2e4f719f946f7ca8d16a188f52f28f07
consultoria-panovo <consultoria@panovo.mx>
2026-03-12 10:31:16 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const sql = require("mssql");
+const app = express();
+
+// Configuración desde variables de ambiente
+const config = {
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  server: process.env.DB_SERVER,
+  database: process.env.DB_DATABASE,
+  options: {
+    encrypt: true,
+    trustServerCertificate: false
+  }
+};
+
+// Endpoint para leer Recetas 
+app.get("/Recetas", async (req, res) => {
+  try {
+    const pool = await sql.connect(config);
+
+    const result = await pool.request().query(`
+      SELECT MATNR_PADRE, DESC_PADRE, WERKS, MATNR_COMPONENTE, MENGE, MEINS, BMENG,BMEIN
+      FROM Recetas
+    `);
+
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.json(result.recordset);
+  } catch (err) {
+    res.status(500).json({ error: err.toString() });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("API Recetas corriendo en puerto 3000");
+});
